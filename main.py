dcd25e4c0cad9b33eae506e37088fe6df60e9702
syzen08 <die.nudel@aol.com>
2024-08-04 20:40:27 +0200

    fix logging

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ import discord
 
 from bot import PredictionBot
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+logger = logging.getLogger('discord')
+logger.setLevel(logging.INFO)
 handler = logging.StreamHandler(stream=sys.stdout)
 formatter = coloredlogs.ColoredFormatter(fmt="%(asctime)s [%(levelname)s] - %(name)s: %(message)s")
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
+
 with open("token.txt") as f:
     token = f.readlines()[0]
-
 
 intents = discord.Intents.default()
 
